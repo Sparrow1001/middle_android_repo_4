618330a6477ff526258643b29c6f10e734214dc3
Evgeniy Fedorenko <evgeny.fedorenko@aliexpress.ru>
2025-03-30 02:40:39 +0300

    sprint 4

--- app/src/main/java/com/yandex/practicum/middle_homework_4/data/setting_repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/com/yandex/practicum/middle_homework_4/data/setting_repository/SettingsRepositoryImpl.kt
@@ -1,12 +1,11 @@
 package com.yandex.practicum.middle_homework_4.data.setting_repository
 
-import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.MutablePreferences
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
+import com.yandex.practicum.middle_homework_4.data.setting_repository.SettingContainer.Companion.DEFAULT_REFRESH_PERIOD
+import com.yandex.practicum.middle_homework_4.data.setting_repository.SettingContainer.Companion.FIST_LAUNCH_DELAY
 import com.yandex.practicum.middle_homework_4.ui.contract.SettingsRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
@@ -14,6 +13,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -34,20 +34,25 @@ class SettingsRepositoryImpl(
 
     override suspend fun saveSetting(periodic: Long, delayed: Long) {
         withContext(dispatcher) {
-            // Реализуйте функционал записи в dataStore
-            // Для periodic ключ - REFRESH_PERIOD_KEY
-            // Для delayed ключ - FIRST_LAUNCH_DELAY_KEY
-            // После записи данных обновите _state
+            dataStore.edit { preferences ->
+                preferences[REFRESH_PERIOD_KEY] = periodic
+                preferences[FIRST_LAUNCH_DELAY_KEY] = delayed
+                updateState(periodic, delayed)
+            }
         }
     }
 
-
     override suspend fun readSetting() {
-        withContext(dispatcher){
-            // Реализуйте функционал чтения данных  из dataStore.
-            // Для periodic ключ - REFRESH_PERIOD_KEY, значение по умолчанию SettingContainer.DEFAULT_REFRESH_PERIOD
-            // Для delayed ключ - FIRST_LAUNCH_DELAY_KEY, значение по умолчанию SettingContainer.FIST_LAUNCH_DELAY
-            // После чтения данных обновите _state
+        withContext(dispatcher) {
+            dataStore.data.collect { pref ->
+                val periodic = pref[REFRESH_PERIOD_KEY] ?: DEFAULT_REFRESH_PERIOD
+                val delayed = pref[FIRST_LAUNCH_DELAY_KEY] ?: FIST_LAUNCH_DELAY
+                updateState(periodic, delayed)
+            }
         }
+    }
+
+    private fun updateState(periodic: Long, delayed: Long) {
+        _state.update { it.copy(periodic = periodic, delayed = delayed) }
     }
 }
